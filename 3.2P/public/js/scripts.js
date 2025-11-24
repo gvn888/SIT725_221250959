9385a23b6b06129b4bf00452b0fcda4f0d6e7bbd
gvn888 <gavj893@gmail.com>
2025-11-24 13:03:23 +1100

    before adding modal

--- 3.2P/public/js/scripts.js
+++ 3.2P/public/js/scripts.js
@@ -1,5 +1,35 @@
+const cardList = [
+    {
+        title: "Windows 7",
+        image: "images/windows-7-logo.png",
+        link: "About Windows 7",
+        description: "Brief info about Windows 7"
+    },
+    {
+        title: "Windows Longhorn",
+        image: "images/windows-longhorn-logo.png",
+        link: "About Windows Longhorn",
+        description: "Brief info about Windows Longhorn"
+    }
+]
+
+
 const clickMe = () => {
     alert("You clicked it.")
+}
+
+const addCards = (items) => {
+    items.forEach(item => {
+        let itemToAppend = '<div class=col s4 center-align">'+
+        '<div class="card medium"><div class="card-image waves-effect waves-block waves-light"><img class="activator" src="'+item.image+'">'+
+        '</div><div class="card-content">'+
+        '<span class="card-title activator grey-text text-darken-4">'+item.title+'<i class="material-icons right">more_vert</i></span><p><a href="#">'+item.link+'</a></p></div>'+
+        '<div class="card-reveal">'+
+            '<span class="card-title grey-text text-darken-4">'+item.title+'<i class="material-icons right">close</i></span>'+
+            '<p class="card-text">'+item.description+'</p>'+
+        '</div></div></div>';
+        $("#card-section").append(itemToAppend)
+    });
 }
 
 $(document).ready(function() {
@@ -7,4 +37,5 @@ $(document).ready(function() {
     $('#clickMeButton').click(() => {
         clickMe();
     })
-})
+    addCards(cardList);
+});
